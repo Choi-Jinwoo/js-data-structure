6af2ac4d6cbea52dc546775cb6703856798492f7
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-01 17:31:19 +0900

    comment: Stack description comment

--- Stack/index.js
+++ Stack/index.js
@@ -1,36 +1,42 @@
 /**
  * Stack
- * - FILO (Fist In Last Out)
+ * push: O(1)
+ * pop: O(1)
+ * LIFO(Last In First Out) 특징
  */
-
 class Stack {
-  #top = 0;
-
   constructor() {
     this.data = [];
+    this.top = 0;
   }
 
+  /**
+   * @description O(1)의 시간 복잡도
+   * @param {number} data - 삽입할 데이터
+   */
   push(data) {
-    this.data[this.#top++] = data;
+    this.data[this.top++] = data;
   }
 
+  /**
+   * @description O(1)의 시간 복잡도
+   */
   pop() {
-    if (this.#top === 0) {
-      return null;
+    if (this.top === 0) {
+      throw new Error("Stack Underflow");
     }
 
-    return this.data[--this.#top];
+    this.data.length -= 1;
+    return this.data[--this.top];
   }
 }
 
 const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.push(4);
+stack.push(1); // [1]
+stack.push(2); // [1, 2]
+stack.push(3); // [1, 2, 3]
+stack.push(4); // [1, 2, 3, 4]
+stack.pop(); // [1, 2, 3]
+stack.pop(); // [1, 2]
 
-console.log(stack.pop()); // 4
-console.log(stack.pop()); // 3
-console.log(stack.pop()); // 2
-console.log(stack.pop()); // 1
-console.log(stack.pop()); // null
+console.log(stack.data);
