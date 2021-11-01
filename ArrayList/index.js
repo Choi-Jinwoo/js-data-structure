2f01c75bb783b6502b2f434554c1d218a97f3fc2
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-01 17:00:31 +0900

    refactor: ArrayList middle insert and add comments

--- ArrayList/index.js
+++ ArrayList/index.js
@@ -1,41 +1,52 @@
 /**
  * ArrayList
- * - index를 통해 접근
- * - 삽입, 삭제의 비용이 높음
+ * 조회: O(1)
+ * 삽입/삭제: O(n)
  */
-
 class ArrayList {
-  #data;
-
-  constructor(data) {
-    this.#data = data ?? [];
+  constructor() {
+    this.data = [];
   }
 
-  items() {
-    return this.#data;
+  /**
+   * @description O(1)의 시간 복잡도
+   * @param {number} index - 조회할 인덱스
+   */
+  get(index) {
+    return this.data[index];
   }
 
-  get(i) {
-    return this.#data[i];
-  }
-
-  add(data) {
-    this.#data[this.#data.length] = data;
-  }
-
-  remove(index) {
-    for (let i = index; i < this.#data.length - 1; i += 1) {
-      this.#data[i] = this.#data[i + 1];
+  /**
+   * @description O(n)의 시간 복잡도
+   * @param {number} index - 삽입할 인덱스 위치
+   * @param {number} value - 삽입할 데이터
+   */
+  add(index, value) {
+    this.data.length += 1;
+    for (let i = this.data.length - 1; i > index; i -= 1) {
+      this.data[i] = this.data[i - 1];
     }
 
-    this.#data.length -= 1;
+    this.data[index] = value;
+  }
+
+  /**
+   * @description O(n)의 시간 복잡도
+   * @param {number} index - 삭제할 데이터 인덱스
+   */
+  remove(index) {
+    for (let i = index; i < this.data.length - 1; i += 1) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.data.length -= 1;
   }
 }
 
-const array = new ArrayList([1, 2, 3, 4]);
+const arrayList = new ArrayList();
+arrayList.add(0, 1); // [1]
+arrayList.add(1, 2); // [1, 2]
+arrayList.add(2, 3); // [1, 2, 3]
+arrayList.add(1, 4); // [1, 4, 2, 3]
+arrayList.remove(1); // [1, 2, 3]
 
-console.log(array.items()); // [1, 2, 3, 4]
-array.add(5);
-console.log(array.items()); // [1, 2, 3, 4, 5]
-array.remove(2);
-console.log(array.items()); // [1, 2, 4, 5]
+console.log(arrayList);
