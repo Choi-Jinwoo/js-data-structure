e85d18ce9189862ef1d14e4041e2d1b44ebc7e14
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-01 19:25:00 +0900

    add: Tree structure

---
+++ Tree/index.js
@@ -0,0 +1,19 @@
+/**
+ * Tree
+ */
+class Node {
+  constructor(data, parent) {
+    this.data = data;
+    this.parent = parent;
+  }
+}
+
+const parent = new Node(1, null);
+const child1 = new Node(2, parent);
+const child2 = new Node(3, parent);
+
+/**
+ *     parent (1)
+ *      /      \
+ * child(2)  child(3)
+ */
